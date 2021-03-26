d3ef2ce0e5de08668e860b9a3616afefe981fb87
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-03-26 21:26:07 +0530

    Program 1

---
+++ src/com/streamliners/task0/Main.java
@@ -0,0 +1,15 @@
+package com.streamliners.task0;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Java Program to Print an Integer (Entered by the User)
+
+        Scanner sc = new Scanner(System.in);   // create a scanner object
+        System.out.print("Enter a Number ");
+        int a = sc.nextInt();                  //  Read user input
+        System.out.println("Entered Number : " + a );
+
+    }
+}
