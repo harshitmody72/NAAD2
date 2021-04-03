71b4ebd8bbefa25bcef8d8c05e6e224a54929089
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-04-03 09:02:24 +0530

    Day 9

---
+++ src/com/streamliners/task0/Strings/CompareStrings.java
@@ -0,0 +1,14 @@
+package com.streamliners.task0.Strings;
+
+public class CompareStrings {
+    public static void main(String[] args) {
+        //Java Program to Compare Strings
+        String str1 = "Harshit";
+        String str2 = "Harshit";
+
+        if (str1.equals(str2))
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
