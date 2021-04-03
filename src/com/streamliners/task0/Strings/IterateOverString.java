e37a16475f080ed316c771e5c731aa7ce5184e70
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-04-03 12:28:38 +0530

    Day 9

---
+++ src/com/streamliners/task0/Strings/IterateOverString.java
@@ -0,0 +1,13 @@
+package com.streamliners.task0.Strings;
+
+public class IterateOverString {
+    public static void main(String[] args) {
+        //Java Program to Iterate through each characters of the string.
+
+        String str = "harshit";
+
+        for(int i = 0; i<str.length();i++)
+            System.out.print(str.charAt(i) +" , ");
+
+    }
+}
