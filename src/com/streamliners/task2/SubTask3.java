0c13f3ae217b27c8b48f434a57741f8e66b8223e
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-04-07 11:24:47 +0530

    2.3 game

--- src/com/streamliners/task2/SubTask3.java
+++ src/com/streamliners/task2/SubTask3.java
@@ -1,13 +1,53 @@
 package com.streamliners.task2;
 
-public class SubTask3 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        startGame("SHAMBHO");
+public class SubTask3 {
+    public static void StartGame(String s) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i)=='A'||s.charAt(i)=='E'||s.charAt(i)=='I'||s.charAt(i)=='O'||s.charAt(i)=='U'){
+                char randomChar = s.charAt(i);
+                sb.append(randomChar);
+            }
+            else {
+                count++;
+                sb.append('_');
+            }
+        }
+        int chances = count + 3;
+        System.out.println(sb.toString());
+        String update = sb.toString();
+        while (chances!=0){
+            System.out.println("Chances :" + chances);
+            char g;
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Guess: ");
+            g = sc.next().charAt(0);
+
+            int counting=0;
+            for (int i = 0; i <update.length() ; i++) {
+                if (g==s.charAt(i)){
+                    update = update.substring(0,i) + g + update.substring(i+1);
+                    counting++;
+                }
+            }
+            if (update.equals(s)){
+                System.out.println("YOU WON!!");
+                return ;
+            }
+            System.out.println("Character found " + counting + " times.");
+            chances--;
+        }
+        System.out.println("YOU LOST THE GAME");
     }
 
-    private static void startGame(String s) {
-        //Write your code here!
+    public static void main(String[] args) {
+        System.out.println("Enter a name: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        StartGame(s);
     }
 
 }
