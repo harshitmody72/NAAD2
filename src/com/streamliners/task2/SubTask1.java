c254534db8d3d7c32d2fe465bcc0d83ddb2c0df0
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-04-07 11:17:11 +0530

    2.1 getlist()

--- src/com/streamliners/task2/SubTask1.java
+++ src/com/streamliners/task2/SubTask1.java
@@ -1,17 +1,24 @@
 package com.streamliners.task2;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubTask1 {
 
     public static void main(String[] args) {
+
         System.out.println(getList("1;2;3;4"));
     }
 
     private static List<Integer> getList(String s) {
         //Write your code here!
+        ArrayList<Integer> List = new ArrayList<>();
+        String[] arrstr = s.split(";");
+        for(int i = 0 ; i<arrstr.length;i++){
+            List.add(Integer.parseInt(arrstr[i]));
+        }
 
-        return null;
+        return List;
     }
 
 }
